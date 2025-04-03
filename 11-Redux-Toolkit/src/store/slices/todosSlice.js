55ae09ad7f9fc2678c897081c6ef3a72b2fd5d0f
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-04-03 12:25:45 +0500

    Improve the Quality

--- 11-Redux-Toolkit/src/store/slices/todosSlice.js
+++ 11-Redux-Toolkit/src/store/slices/todosSlice.js
@@ -8,10 +8,14 @@ export const todosSlice = createSlice({
     reducers: {
         addTodos: (state, action) => {
             if (action.payload.length > 1) {
-                state.todos = [...state.todos, {
-                    items: action.payload.trim(),
-                    id: nanoid(),
-                }];
+                if (state.todos.find((item) => action.payload.trim() == item.items)) {
+                    alert("This Item Already Added")
+                } else {
+                    state.todos = [...state.todos, {
+                        items: action.payload.trim(),
+                        id: nanoid(),
+                    }];
+                }
             } else {
                 alert("Please Enter a valid Items")
             }
@@ -19,6 +23,13 @@ export const todosSlice = createSlice({
         deleteTodos: (state, action) => {
             state.todos.splice(action.payload, 1);
             state.todos = [...state.todos]
+        },
+        editTodos: (state, action) => {
+            if (action.payload.length > 1) {
+                state.todos.splice(action.payload.index, 1, action.payload.newValue);
+            } else {
+                alert("Please Enter a valid Items")
+            }
         }
     },
 
